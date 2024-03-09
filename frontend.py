dc9e904150404ad098841a8ccdad67b5f31aa082
leeyinghern <leeyinghern@gmail.com>
2024-03-09 13:35:20 +0900

    refactor: Updated frontend to st.chat_message method

--- frontend.py
+++ frontend.py
@@ -1,21 +1,53 @@
 import streamlit as st
 import httpx
+import time
 
 st.title("Code Generator")
 
-# User input form
-user_input = st.text_area("Describe your coding problem:")
+# To create streaming effect
 
-if st.button("Generate Code"):
+
+def streaming_effect(message):
+    for word in message.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+# Streamed response emulator
+def response_generator(user_input):
     with httpx.Client() as client:
         response = client.post(
             "http://127.0.0.1:8000/generate_code",
             json={"description": user_input},  # noqa
         )
 
-    # Display generated code
     if response.status_code == 200:
         generated_code = response.json()["code"]
-        st.code(generated_code, language="python")
-    else:
-        st.error(f"Error generating code. Status code: {response.status_code}")
+        generated_message = response.json()["message"]
+
+        return generated_code, generated_message
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Accept user input
+if prompt := st.chat_input("Input coding problem description"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        code, message = response_generator(prompt)
+        st.write_stream(streaming_effect(message))
+        st.code(code, language="python")
+
+    st.session_state.messages.append({"role": "assistant", "content": code})
--- main.py
+++ main.py
@@ -8,8 +8,20 @@ class CodeRequest(BaseModel):
     description: str
 
 
+class FeedbackRequest(BaseModel):
+    code: str
+    feedback: str
+
+
 @app.post("/generate_code")
 def generate_code(request: CodeRequest):
     # Code for LLM to generate code
     generated_code = f"Generated code for: {request.description}"
-    return {"code": generated_code}
+    return {"code": generated_code, "message": "Updated code"}
+
+
+@app.post("/submit_feedback")
+def submit_feedback(request: FeedbackRequest):
+    print(f"Received feedback: {request.feedback}")
+    print(f"Generated code: {request.code}")
+    return {"message": "Feedback received and noted."}
